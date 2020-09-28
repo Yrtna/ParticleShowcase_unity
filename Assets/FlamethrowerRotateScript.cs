3f8a116ea398e17484084ae78810071fc67a34d1
Yrtna <dummyauction42@gmail.com>
2020-09-28 13:27:31 +0200

    Initial project with around finished 10 particle systems

---
+++ Assets/FlamethrowerRotateScript.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlamethrowerRotateScript : MonoBehaviour
+{
+    public float RotationSpeed;
+    public float Angle;
+    public float CycleTime;
+
+    private bool goLeft;
+    private float currentCycleTime;
+
+    private void Start()
+    {
+        currentCycleTime = CycleTime;
+    }
+
+    void Update()
+    {
+        currentCycleTime -= Time.deltaTime;
+        if (currentCycleTime < 0)
+        {
+            goLeft = !goLeft;
+            currentCycleTime = CycleTime;
+        }
+        
+        if (goLeft)
+            transform.Rotate(Vector3.up, -Angle * RotationSpeed * Time.deltaTime);
+        else
+            transform.Rotate(Vector3.up, Angle * RotationSpeed * Time.deltaTime);
+    }
+}
